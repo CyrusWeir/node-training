3bfa8a45199494c9364f6a11bb27974a440877d4
CyrusWeir <cyrusweir@gmail.com>
2024-10-15 10:59:27 +0200

    created files, and prepared them to push

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.bgBlue("Naima"));
+console.log(chalk.bgCyan("Alice"));
+console.log(chalk.bgGreen("Clément"));
+console.log(chalk.bgYellowBright("Cyrus"));
